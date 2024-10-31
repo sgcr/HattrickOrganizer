d4e1c02bd8fa57a62110d8e6dfedda2a9512c988
Sebastian Reddig <sgcr@gmx.net>
2024-10-31 19:23:32 +0100

    refactor: Making inner classes static

--- src/main/java/module/tsforecast/Curve.java
+++ src/main/java/module/tsforecast/Curve.java
@@ -55,7 +55,7 @@ class Curve {
 	private Point m_currentPoint = null;
 	private Color m_Color = null;
 
-	class Point implements Comparable<Point> {
+	static class Point implements Comparable<Point> {
 
 		double m_dSpirit;
 		int m_iAttitude;
