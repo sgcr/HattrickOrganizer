40440ef3c653993fa12346e4e6b9bad039c2b1da
Sebastian Reddig <sgcr@gmx.net>
2026-01-11 17:39:36 +0100

    feat: Improve performance of enum value lookups

--- src/main/java/core/model/cup/CupLevelIndex.java
+++ src/main/java/core/model/cup/CupLevelIndex.java
@@ -1,36 +1,29 @@
 package core.model.cup;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+@Getter
+@RequiredArgsConstructor
 public enum CupLevelIndex {
 
-    NONE(0), // i.e MatchType is not 3.
+    NONE(0), // i.e. MatchType is not 3.
     EMERALD(1),  // EMERALD, National or Divisional cup
     RUBY(2), // Ruby cup
     SAPPHIRE(3);  // Sapphire cup
 
     private final int id;
-    private static final Map<Integer, CupLevelIndex> map = new HashMap<>();
 
-    CupLevelIndex(int id) {
-        this.id = id;
-    }
-
-    static {
-        for (CupLevelIndex oCupLevelIndex : CupLevelIndex.values()) {
-            map.put(oCupLevelIndex.id, oCupLevelIndex);
-        }
-    }
+    private static final Map<Integer, CupLevelIndex> MAP_ID_TO_CUP_LEVEL_INDEX =
+        Arrays.stream(values()).collect(Collectors.toMap(CupLevelIndex::getId, Function.identity()));
 
     public static CupLevelIndex fromInt(Integer iCupLevelIndex) {
-        if ( iCupLevelIndex != null) {
-            return (CupLevelIndex) map.get(iCupLevelIndex);
-        }
-        return null;
-    }
-
-    public int getId() {
-        return id;
+        return MAP_ID_TO_CUP_LEVEL_INDEX.getOrDefault(iCupLevelIndex, null);
     }
 }
