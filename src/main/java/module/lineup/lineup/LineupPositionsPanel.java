f6d8699de3acee25583b068f19f2ed9bf4332021
Sebastian Reddig <sgcr@gmx.net>
2026-01-23 11:54:28 +0100

    feat: #2395: Match Orders: Behavior of players for substitutes' bench

--- src/main/java/module/lineup/lineup/LineupPositionsPanel.java
+++ src/main/java/module/lineup/lineup/LineupPositionsPanel.java
@@ -36,11 +36,11 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 	//private MatchBanner m_jpMatchBanner;
 	private final JButton m_jbFlipSide = new JButton(ThemeManager.getIcon(HOIconName.RELOAD));
 	private PlayerPositionPanel m_clKeeper;
-	private PlayerPositionPanel m_clLeftBack;
+	private PlayerPositionPanel m_clLeftWingback;
 	private PlayerPositionPanel m_clLeftCentralDefender;
 	private PlayerPositionPanel m_clMiddleCentralDefender;
 	private PlayerPositionPanel m_clRightCentralDefender;
-	private PlayerPositionPanel m_clRightBack;
+	private PlayerPositionPanel m_clRightWingback;
 	private PlayerPositionPanel m_clLeftWinger;
 	private PlayerPositionPanel m_clLeftInnerMidfielder;
 	private PlayerPositionPanel m_clCentralInnerMidfielder;
@@ -51,21 +51,21 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 	private PlayerPositionPanel m_clRightForward;
 	private PlayerPositionPanel m_clSubstKeeper1;
 	private PlayerPositionPanel m_clSubstKeeper2;
-	private PlayerPositionPanel m_clSubstCD1;
-	private PlayerPositionPanel m_clSubstCD2;
-	private PlayerPositionPanel m_clSubstWB1;
-	private PlayerPositionPanel m_clSubstWB2;
-	private PlayerPositionPanel m_clSubstIM1;
-	private PlayerPositionPanel m_clSubstIM2;
-	private PlayerPositionPanel m_clSubstFwd1;
-	private PlayerPositionPanel m_clSubstFwd2;
-	private PlayerPositionPanel m_clSubstWI1;
-	private PlayerPositionPanel m_clSubstWI2;
-	private PlayerPositionPanel m_clSubstXtr1;
-	private PlayerPositionPanel m_clSubstXtr2;
+	private PlayerPositionPanel m_clSubstCentralDefender1;
+	private PlayerPositionPanel m_clSubstCentralDefender2;
+	private PlayerPositionPanel m_clSubstWingback1;
+	private PlayerPositionPanel m_clSubstWingback2;
+	private PlayerPositionPanel m_clSubstInnerMidfielder1;
+	private PlayerPositionPanel m_clSubstInnerMidfielder2;
+	private PlayerPositionPanel m_clSubstForward1;
+	private PlayerPositionPanel m_clSubstForward2;
+	private PlayerPositionPanel m_clSubstWinger1;
+	private PlayerPositionPanel m_clSubstWinger2;
+	private PlayerPositionPanel m_clSubstExtra1;
+	private PlayerPositionPanel m_clSubstExtra2;
 	private PlayerPositionPanel m_clCaptain;
 	private PlayerPositionPanel m_clSetPieceTaker;
-	private javax.swing.JLayeredPane centerPanel;
+	private JLayeredPane centerPanel;
 	private final SwapPositionsManager swapPositionsManager = new SwapPositionsManager(this);
 
 	private Weather m_weather;
@@ -86,7 +86,7 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		RefreshManager.instance().registerRefreshable(this);
 	}
 
-	public javax.swing.JLayeredPane getCenterPanel() {
+	public JLayeredPane getCenterPanel() {
 		return centerPanel;
 	}
 
@@ -158,11 +158,11 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		}
 
 		m_clKeeper.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clLeftBack.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clLeftWingback.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clLeftCentralDefender.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clMiddleCentralDefender.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clRightCentralDefender.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clRightBack.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clRightWingback.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clLeftWinger.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clLeftInnerMidfielder.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clCentralInnerMidfielder.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
@@ -173,18 +173,18 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		m_clRightForward.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clSubstKeeper1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
 		m_clSubstKeeper2.refresh2(filteredPlayers, m_clSubstKeeper1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstCD1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clSubstCD2.refresh2(filteredPlayers, m_clSubstCD1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstWB1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clSubstWB2.refresh2(filteredPlayers, m_clSubstWB1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstIM1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clSubstIM2.refresh2(filteredPlayers, m_clSubstIM1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstFwd1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clSubstFwd2.refresh2(filteredPlayers, m_clSubstFwd1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstWI1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
-		m_clSubstWI2.refresh2(filteredPlayers, m_clSubstWI1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
-		m_clSubstXtr1.refresh(filteredPlayers, selectedPlayers, substitutes, null, false, matchMinute);
-		m_clSubstXtr2.refresh2(filteredPlayers, m_clSubstXtr1.getiSelectedPlayerId(), null, false);
+		m_clSubstCentralDefender1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clSubstCentralDefender2.refresh2(filteredPlayers, m_clSubstCentralDefender1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
+		m_clSubstWingback1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clSubstWingback2.refresh2(filteredPlayers, m_clSubstWingback1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
+		m_clSubstInnerMidfielder1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clSubstInnerMidfielder2.refresh2(filteredPlayers, m_clSubstInnerMidfielder1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
+		m_clSubstForward1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clSubstForward2.refresh2(filteredPlayers, m_clSubstForward1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
+		m_clSubstWinger1.refresh(filteredPlayers, selectedPlayers, substitutes, m_weather, m_useWeatherImpact, matchMinute);
+		m_clSubstWinger2.refresh2(filteredPlayers, m_clSubstWinger1.getiSelectedPlayerId(), m_weather, m_useWeatherImpact);
+		m_clSubstExtra1.refresh(filteredPlayers, selectedPlayers, substitutes, null, false, matchMinute);
+		m_clSubstExtra2.refresh2(filteredPlayers, m_clSubstExtra1.getiSelectedPlayerId(), null, false);
 		m_clSetPieceTaker.refresh(selectedPlayers, null, null, m_weather, m_useWeatherImpact, matchMinute);
 		m_clCaptain.refresh(selectedPlayers, null, null, null, false, matchMinute);
 
@@ -202,7 +202,7 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 	private void initComponents() {
 		setLayout(new BorderLayout());
 
-		centerPanel = new javax.swing.JLayeredPane();
+		centerPanel = new JLayeredPane();
 		centerPanel.setOpaque(false);
 
 		final GridBagLayout layout = new GridBagLayout();
@@ -231,11 +231,11 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		constraints.gridy = getLineupRowNumber(1);
 		constraints.insets = new Insets(3, 3, 3, 3);
 		//constraints.anchor = GridBagConstraints.CENTER;
-		m_clRightBack = new PlayerPositionPanel(this, IMatchRoleID.rightBack, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clRightBack, constraints);
-		centerPanel.add(m_clRightBack);
-		swapPositionsManager.addSwapCapabilityTo(m_clRightBack);
-		m_clLineupPanel.addToAssistant(m_clRightBack);
+		m_clRightWingback = new PlayerPositionPanel(this, IMatchRoleID.rightBack, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clRightWingback, constraints);
+		centerPanel.add(m_clRightWingback);
+		swapPositionsManager.addSwapCapabilityTo(m_clRightWingback);
+		m_clLineupPanel.addToAssistant(m_clRightWingback);
 
 		// Defense line
 		constraints.gridx = 1 + getLineupColumnNumber(1); //2;
@@ -263,11 +263,11 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		m_clLineupPanel.addToAssistant(m_clLeftCentralDefender);
 
 		constraints.gridx = 1 + getLineupColumnNumber(4);//5;
-		m_clLeftBack = new PlayerPositionPanel(this, IMatchRoleID.leftBack, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clLeftBack, constraints);
-		centerPanel.add(m_clLeftBack);
-		swapPositionsManager.addSwapCapabilityTo(m_clLeftBack);
-		m_clLineupPanel.addToAssistant(m_clLeftBack);
+		m_clLeftWingback = new PlayerPositionPanel(this, IMatchRoleID.leftBack, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clLeftWingback, constraints);
+		centerPanel.add(m_clLeftWingback);
+		swapPositionsManager.addSwapCapabilityTo(m_clLeftWingback);
+		m_clLineupPanel.addToAssistant(m_clLeftWingback);
 
 		// Midfield Line
 		constraints.gridx = 1 + getLineupColumnNumber(0); //1;
@@ -331,7 +331,7 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		swapPositionsManager.addSwapCapabilityTo(m_clLeftForward);
 		m_clLineupPanel.addToAssistant(m_clLeftForward);
 
-		// Captain and setpieces
+		// Captain and set pieces
 		constraints.gridx = 0;
 		constraints.gridy = 1;
 		constraints.anchor = GridBagConstraints.SOUTH;
@@ -347,7 +347,7 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		layout.setConstraints(m_clSetPieceTaker, constraints);
 		centerPanel.add(m_clSetPieceTaker);
 
-		// Buttons to allocate team number to players currently on the line up
+		// Buttons to allocate team number to players currently on the line-up
 		constraints.gridx = 0;
 		constraints.gridy = 3;
 		constraints.gridwidth = 2;
@@ -381,40 +381,40 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		swapPositionsManager.addSwapCapabilityTo(m_clSubstKeeper1);
 
 		constraints.gridx = 1;
-		m_clSubstCD1 = new PlayerPositionPanel(this, IMatchRoleID.substCD1, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstCD1, constraints);
-		centerPanel.add(m_clSubstCD1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstCD1);
+		m_clSubstCentralDefender1 = new PlayerPositionPanel(this, IMatchRoleID.substCD1, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstCentralDefender1, constraints);
+		centerPanel.add(m_clSubstCentralDefender1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstCentralDefender1);
 
 		constraints.gridx = 2;
-		m_clSubstWB1 = new PlayerPositionPanel(this, IMatchRoleID.substWB1, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstWB1, constraints);
-		centerPanel.add(m_clSubstWB1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstWB1);
+		m_clSubstWingback1 = new PlayerPositionPanel(this, IMatchRoleID.substWB1, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstWingback1, constraints);
+		centerPanel.add(m_clSubstWingback1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstWingback1);
 
 		constraints.gridx = 3;
-		m_clSubstIM1 = new PlayerPositionPanel(this, IMatchRoleID.substIM1, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstIM1, constraints);
-		centerPanel.add(m_clSubstIM1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstIM1);
+		m_clSubstInnerMidfielder1 = new PlayerPositionPanel(this, IMatchRoleID.substIM1, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstInnerMidfielder1, constraints);
+		centerPanel.add(m_clSubstInnerMidfielder1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstInnerMidfielder1);
 
 		constraints.gridx = 4;
-		m_clSubstFwd1 = new PlayerPositionPanel(this, IMatchRoleID.substFW1, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstFwd1, constraints);
-		centerPanel.add(m_clSubstFwd1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstFwd1);
+		m_clSubstForward1 = new PlayerPositionPanel(this, IMatchRoleID.substFW1, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstForward1, constraints);
+		centerPanel.add(m_clSubstForward1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstForward1);
 
 		constraints.gridx = 5;
-		m_clSubstWI1 = new PlayerPositionPanel(this, IMatchRoleID.substWI1, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstWI1, constraints);
-		centerPanel.add(m_clSubstWI1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstWI1);
+		m_clSubstWinger1 = new PlayerPositionPanel(this, IMatchRoleID.substWI1, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstWinger1, constraints);
+		centerPanel.add(m_clSubstWinger1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstWinger1);
 
 		constraints.gridx = 6;
-		m_clSubstXtr1 = new PlayerPositionPanel(this, IMatchRoleID.substXT1);
-		layout.setConstraints(m_clSubstXtr1, constraints);
-		centerPanel.add(m_clSubstXtr1);
-		swapPositionsManager.addSwapCapabilityTo(m_clSubstXtr1);
+		m_clSubstExtra1 = new PlayerPositionPanel(this, IMatchRoleID.substXT1);
+		layout.setConstraints(m_clSubstExtra1, constraints);
+		centerPanel.add(m_clSubstExtra1);
+		swapPositionsManager.addSwapCapabilityTo(m_clSubstExtra1);
 
 		constraints.gridx = 0;
 		constraints.gridy = 6;
@@ -423,34 +423,34 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		centerPanel.add(m_clSubstKeeper2);
 
 		constraints.gridx = 1;
-		m_clSubstCD2 = new PlayerPositionPanel(this, IMatchRoleID.substCD2, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstCD2, constraints);
-		centerPanel.add(m_clSubstCD2);
+		m_clSubstCentralDefender2 = new PlayerPositionPanel(this, IMatchRoleID.substCD2, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstCentralDefender2, constraints);
+		centerPanel.add(m_clSubstCentralDefender2);
 
 		constraints.gridx = 2;
-		m_clSubstWB2 = new PlayerPositionPanel(this, IMatchRoleID.substWB2, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstWB2, constraints);
-		centerPanel.add(m_clSubstWB2);
+		m_clSubstWingback2 = new PlayerPositionPanel(this, IMatchRoleID.substWB2, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstWingback2, constraints);
+		centerPanel.add(m_clSubstWingback2);
 
 		constraints.gridx = 3;
-		m_clSubstIM2 = new PlayerPositionPanel(this, IMatchRoleID.substIM2, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstIM2, constraints);
-		centerPanel.add(m_clSubstIM2);
+		m_clSubstInnerMidfielder2 = new PlayerPositionPanel(this, IMatchRoleID.substIM2, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstInnerMidfielder2, constraints);
+		centerPanel.add(m_clSubstInnerMidfielder2);
 
 		constraints.gridx = 4;
-		m_clSubstFwd2 = new PlayerPositionPanel(this, IMatchRoleID.substFW2, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstFwd2, constraints);
-		centerPanel.add(m_clSubstFwd2);
+		m_clSubstForward2 = new PlayerPositionPanel(this, IMatchRoleID.substFW2, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstForward2, constraints);
+		centerPanel.add(m_clSubstForward2);
 
 		constraints.gridx = 5;
-		m_clSubstWI2 = new PlayerPositionPanel(this, IMatchRoleID.substWI2, m_weather, m_useWeatherImpact, matchMinute);
-		layout.setConstraints(m_clSubstWI2, constraints);
-		centerPanel.add(m_clSubstWI2);
+		m_clSubstWinger2 = new PlayerPositionPanel(this, IMatchRoleID.substWI2, m_weather, m_useWeatherImpact, matchMinute);
+		layout.setConstraints(m_clSubstWinger2, constraints);
+		centerPanel.add(m_clSubstWinger2);
 
 		constraints.gridx = 6;
-		m_clSubstXtr2 = new PlayerPositionPanel(this, IMatchRoleID.substXT2);
-		layout.setConstraints(m_clSubstXtr2, constraints);
-		centerPanel.add(m_clSubstXtr2);
+		m_clSubstExtra2 = new PlayerPositionPanel(this, IMatchRoleID.substXT2);
+		layout.setConstraints(m_clSubstExtra2, constraints);
+		centerPanel.add(m_clSubstExtra2);
 
 		add(centerPanel, BorderLayout.CENTER);
 
@@ -484,13 +484,13 @@ public class LineupPositionsPanel extends core.gui.comp.panel.RasenPanel impleme
 		ArrayList<PlayerPositionPanel> pos = new ArrayList<>(14);
 		pos.add(m_clCentralForward);
 		pos.add(m_clCentralInnerMidfielder);
-		pos.add(m_clLeftBack);
+		pos.add(m_clLeftWingback);
 		pos.add(m_clLeftWinger);
 		pos.add(m_clLeftCentralDefender);
 		pos.add(m_clLeftInnerMidfielder);
 		pos.add(m_clLeftForward);
 		pos.add(m_clMiddleCentralDefender);
-		pos.add(m_clRightBack);
+		pos.add(m_clRightWingback);
 		pos.add(m_clRightWinger);
 		pos.add(m_clRightCentralDefender);
 		pos.add(m_clRightInnerMidfielder);
